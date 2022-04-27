966f19bfa64ba6a9a9c31e31768e8a3be78f08dc
Smarsden007 <seannmarsden@gmail.com>
2022-04-26 22:36:58 -0700

    Almost done building out login and out pages

--- src/App.js
+++ src/App.js
@@ -1,51 +1,39 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom';
 
-const Login = () => {
-  return (<div>
-    <h1>Login</h1>
-      <form>
-        <div>
-        <label htmlFor="username">Username</label>
-        <input id="username"/>
-        </div>
-        <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password"/>
-        </div>
-        <button>Submit</button>
-      </form>
-    </div>)
-}
-const FriendsList = () => {
-  return (<h2>FriendsList</h2>)
-}
 
-const AddFriend = () => {
-  return (<h2>AddFriend</h2>)
-}
-
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import AddFriend from './components/AddFriend';
+import Logout from './components/Logout'
 
 function App() {
   return (
     <Router>
-      
-    <div className="App">
-      {/* <h1>Sean's Friend's</h1> */}
-      <Route exact path="/">
-        <Login/>
-      </Route>
-      <Route exact path="/login">
-        <Redirect to="/"></Redirect>
-      </Route>
-      <Route exact path="/friends">
-        <FriendsList/>
-      </Route>
-      <Route exact path="/friends/add">
-        <AddFriend/>
-      </Route>
-    </div>
+
+      <div className="App">
+        <header>
+          <h2>Friends Database</h2>
+          <Link to="login">Login</Link>
+          <Link to="friends">Friends</Link>
+          <Link to="friends/add">Add Friends</Link>
+          <Link to="friends">Log Out</Link>
+
+        </header>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/login">
+          <Redirect to="/"></Redirect>
+        </Route>
+        <Route exact path="/friends">
+          <FriendsList />
+        </Route>
+        <Route exact path="/friends/add">
+          <AddFriend />
+        </Route>
+      </div>
     </Router>
   );
 }
---
+++ src/components/FriendsList.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+
+const FriendsList = () => {
+
+    const [friends, setFriends] = useState([])
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        axios.get('http://localhost:500/api/friends', {
+            headers: {
+                authorization: token
+            }
+        })
+            .then(resp => {
+                setFriends(resp.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }, []);
+
+
+    return (<div>
+        <h2>FriendsList</h2>
+        <ul>
+            {
+                friends.map(friend=> {
+                    return <li>{friend.name} - {friend.age} - {friend.email}</li>
+                })
+            }
+        </ul>
+    </div>)
+}
+
+export default FriendsList;
